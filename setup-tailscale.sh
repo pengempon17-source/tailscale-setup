66f54dcad66a97c6f7423ec1d798b7b93e17ce51
pengempon17-source <pengempon17@gmail.com>
2026-04-14 10:20:27 +0700

    Delete setup-tailscale.sh

--- setup-tailscale.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install Tailscale
-curl -fsSL https://tailscale.com | sh
-
-# Jalankan Tailscale & Aktifkan Fitur SSH Tailscale (Agar lebih mudah login)
-tailscale up --auth-key tskey-auth-kijgpG6sM821CNTRL-qwqvgsaybDP9WfH8P6GZDPbAgNQ9G6Py --ssh
